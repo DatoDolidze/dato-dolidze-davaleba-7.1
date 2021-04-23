dd99924a34307785cf11da1c7136fd2320830e46
dato <daddolidze@gmail.com>
2021-04-23 16:23:18 +0400

    shows

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ if movies == "yes":
     print("we can watch Watchdogs")
     print("we can watch godzilla vs kong")
 else:
-    print("we can watch something else")
+    tvshow=input("do you prefer tv shows ?")
+    if tvshow=="yes":
+        print("we can watch the office")
