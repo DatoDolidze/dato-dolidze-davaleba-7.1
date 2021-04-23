eee2ae1c84b9804795c12c46bc009cf67398c44d
dato <daddolidze@gmail.com>
2021-04-23 16:26:21 +0400

    borat

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ if movies == "yes":
     print("we can watch interstellar")
     print("we can watch Watchdogs")
     print("we can watch godzilla vs kong")
+    print("we can watch borat")
 else:
     print("we can watch something else")
