0db8d71bb6c7746b3f49840c4333027b652405d6
dato <daddolidze@gmail.com>
2021-04-23 16:20:05 +0400

    mesiji

---
+++ main.py
@@ -0,0 +1,7 @@
+movies = input("want to watch some movies ?")
+if movies == "yes":
+    print("we can watch interstellar")
+    print("we can watch Watchdogs")
+    print("we can watch godzilla vs kong")
+else:
+    print("we can watch something else")
